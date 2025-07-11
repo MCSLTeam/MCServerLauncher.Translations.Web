30f650bbc02a45b4beb1fab4e3d84d5d540a295c
Tigercrl <tigercrl@icloud.com>
2025-07-11 16:21:16 +0800

    update: structure

---
+++ scripts/format-locale.js
@@ -0,0 +1,34 @@
+const fs = require("fs");
+
+const paths = ["./locales/"];
+
+for (const path of paths) {
+  fs.readdirSync(path).forEach((file) => {
+    if (file.endsWith(".json")) {
+      const content = fs.readFileSync(path + file, "utf8");
+      const json = JSON.parse(content);
+      const result =
+        JSON.stringify(
+          json,
+          (key, value) => {
+            if (typeof value === "object" && value !== null) {
+              const sortedObj = {};
+              Object.keys(value)
+                .sort()
+                .forEach((key) => {
+                  sortedObj[key] = value[key];
+                });
+              return sortedObj;
+            }
+            return value;
+          },
+          2,
+        ) + "\n";
+      const changed = content !== result;
+      if (changed) fs.writeFileSync(path + file, result);
+      console.log(
+        `Formatted locale: ${path + file}${changed ? "" : " (unchanged)"}`,
+      );
+    }
+  });
+}
